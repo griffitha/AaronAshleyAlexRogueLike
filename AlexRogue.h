c94421c33e586b28e529cfd2640c2fa4acdc0c78
griffitha <keenplatypus75@gmail.com>
2014-04-04 07:58:19 -0400

    Create AlexRogue.h

---
+++ AlexRogue.h
@@ -0,0 +1,49 @@
+#ifndef ROGUE_H_INCLUDED
+#define ROGUE_H_INCLUDED
+
+#include <cstring>
+#include <iostream>
+#include <curses.h>
+
+using namespace std;
+
+class character
+{
+public:
+    character(int x,int y);
+    //spawns character at given location
+    bool moveChar(WINDOW * workingWindow);
+    bool attack();
+    void setXCoordinate(int input);
+    int getXCoordinate();
+    void setYCoordinate(int input);
+    int getYCoordinate();
+    void setHealth(int input);
+    int getHealth();
+    void setMaxHealth(int input);
+    int getMaxHealth();
+    void setMapRep(char input);
+    char getMapRep();
+    void setCharacterName(string input);
+    string getCharacterName();
+    void setLevel(int input);
+    int getLevel();
+    void setExperience(int input);
+    int getExperience();
+
+
+
+private:
+    int xCoordinate;
+    int yCoordinate;
+    int health;
+    int maxHealth;
+    char mapRep;
+    //character used to represent on map
+    string characterName;
+    int level;
+    int experience;
+
+};
+
+#endif // ROGUE_H_INCLUDED

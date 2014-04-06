3c1e7baa15118158f1496c79db04b72f5d301af0
martina3203 <martina3203@gmail.com>
2014-04-05 21:07:23 -0400

    YOU CAN NOW MOVEEEEEEEE
    Display is bugged but you can move within the bounds. I would like to
    conduct all of the options for the player to use such as inventory
    access while in the playerOptions function. Look and view as you please

--- utility.cpp
+++ utility.cpp
@@ -1,11 +1,22 @@
 #include "utility.h"
 
-void readLevel(char symbolArray[1000][1000], int levelNumber)
+
+//Utility Functions CPP
+
+void readLevel(char symbolArray[500][500],character * gameObjectArray[500][500], character &player, int levelNumber)
 {
     //File Data Type where the file will be loaded
     std::ifstream levelFile;
     //Character save for readin
     char currentCharacter = ' ';
+
+    for (int i = 0; i < 500; i++)
+    {
+        for (int q = 0; q < 500; q++)
+        {
+            symbolArray[i][q] == '#';
+        }
+    }
 
     //Select the level to load
     switch(levelNumber)
@@ -38,14 +49,18 @@ void readLevel(char symbolArray[1000][1000], int levelNumber)
         }
         else if (currentCharacter == 'p')
         {
-            symbolArray[x][y] = 'p';
+            //This clears the spot
+            symbolArray[x][y] = ' ';
+            gameObjectArray[x][y] = &player;
+            player.setXCoordinate(x);
+            player.setYCoordinate(y);
             x++;
             //Will fill with pointer information when character is implemented
         }
         else if (currentCharacter == '/')
         {
             //Moves to next roll
-            symbolArray[x][y] = '/';
+            symbolArray[x][y] = '#';
             y++;
             x = 0;
         }
@@ -55,80 +70,108 @@ void readLevel(char symbolArray[1000][1000], int levelNumber)
             x++;
         }
     }
-    //This is a flag in the array when to stop listing data;
-    symbolArray[x][y] = 'q';
     levelFile.close();
     return;
 }
 
-void printWindow(char symbolArray[][1000], WINDOW * workingWindow, WINDOW * statusWindow, WINDOW * message)
+void printWindow(char symbolArray[500][500],character * gameObjectArray[500][500], character player, WINDOW * workingWindow, WINDOW * statusWindow, WINDOW * message)
 {
-    int x = 20;
-    int y = 20;
-    int ch; // Key input variable
-
-    while(1)
-    {
-        int arrayX = 0;
-        int arrayY = 0;
+        int X;
+        int Y;
+        int playerX = player.getXCoordinate();
+        int playerY = player.getYCoordinate();
+        X = playerX - 15;
+        Y = playerY - 11;
+        //These are finding positions in the window
+        int xCounter = 0;
+        int yCounter = 0;
+        bool mapCreated = false;
         //Sets up the map
-        while (symbolArray[arrayX][arrayY] != 'q')
+
+        wclear(workingWindow);
+        wclear(statusWindow);
+        wclear(message);
+
+        while (mapCreated != true)
         {
-            char currentCharacter = symbolArray[arrayX][arrayY];
-            if (currentCharacter != '/')
+            //Checks to make sure it isnt out of bounds
+            if ((X > 0) && (X < 500) && (Y > 0) && (Y <500))
             {
-                //Sets character at this position
-                mvwaddch(workingWindow,arrayY,arrayX,currentCharacter);
-                arrayX++;
+                if (symbolArray[X][Y] == '#')
+                {
+                    //Makes everything a block
+                    mvwaddch(workingWindow,yCounter,xCounter,ACS_BLOCK);
+                }
+                else
+                {
+                    mvwaddch(workingWindow,yCounter,xCounter,symbolArray[X][Y]);
+                }
             }
-            else if (currentCharacter == '/')
-            {
-                //Moves to next roll if it encounters this character
-                arrayY++;
-                arrayX = 0;
-            }
-            //Moves to next column
             else
             {
-                arrayX++;
+                //If out of bounds, make it a block
+                mvwaddch(workingWindow,yCounter,xCounter,ACS_BLOCK);
+            }
+            //Move up to the next column
+            X++;
+            xCounter++;
+
+            if (X == (playerX + 17))
+            {
+                if (Y == (playerY + 11))
+                {
+                    mapCreated = true;
+                }
+                //Move to the next row
+                Y++;
+                yCounter++;
+                //reset column work
+                X = playerX - 15;
+                xCounter = 0;
             }
         }
-
+        mvwaddch(workingWindow,11,15,player.getMapRep());
         wrefresh(workingWindow);
         wrefresh(statusWindow);
         wrefresh(message);
-
-        //mvprintw(y,x "X");
-        wmove(workingWindow,y,x);
-        waddch(workingWindow,'X');
-
-        ch = wgetch(workingWindow);
-        if (ch == KEY_DOWN)
-        {
-            y++;
-            wclear(workingWindow);
-        }
-        if (ch == KEY_UP)
-        {
-            y--;
-            wclear(workingWindow);
-        }
-        if (ch == KEY_LEFT)
-        {
-            x--;
-            wclear(workingWindow);
-        }
-        if (ch == KEY_RIGHT)
-        {
-            x++;
-            wclear(workingWindow);
-        }
-          //refreshes window
-
-        wrefresh(workingWindow);
-        wrefresh(statusWindow);
-        wrefresh(message);
-    }
     return;
 }
 
+void playerTurn(char symbolArray[][500], character * gameObjectArray[500][500], character &player)
+{
+    //Save Player's current position
+    int playerX = player.getXCoordinate();
+    int playerY = player.getYCoordinate();
+
+    int ch = getch();
+    if (ch == KEY_UP)
+    {
+        if (symbolArray[playerX][playerY-1] == ' ')
+        {
+            player.moveChar(ch);
+        }
+    }
+    else if (ch == KEY_DOWN)
+    {
+        if (symbolArray[playerX][playerY+1] == ' ')
+        {
+            player.moveChar(ch);
+        }
+    }
+    else if (ch == KEY_LEFT)
+    {
+        if (symbolArray[playerX-1][playerY] == ' ')
+        {
+            player.moveChar(ch);
+        }
+    }
+    else if (ch == KEY_RIGHT)
+    {
+        if (symbolArray[playerX+1][playerY] == ' ')
+        {
+            player.moveChar(ch);
+        }
+    }
+
+    return;
+}

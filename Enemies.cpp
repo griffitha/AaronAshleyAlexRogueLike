c806ca0ada5687b7da8bf0d2f8b1a4249f2bbd94
griffitha <keenplatypus75@gmail.com>
2014-04-21 10:29:21 -0400

    Update Enemies.cpp

--- Enemies.cpp
+++ Enemies.cpp
@@ -1,5 +1,13 @@
 #include "utility.h"
-
+void enemy::setXpValue(int input)
+{
+    xpValue=input;
+    return;
+}
+int enemy::getXpValue()
+{
+    return xpValue;
+}
 //Health and  Base Damage formulas taken from alpha ver of Desktop Dungeons
 void enemy::createGoblin(int factor)
 {
@@ -27,52 +35,27 @@ void enemy::createBat(int factor)
     setAccuracy(95);
     setLevel(factor);
 }
-void enemy::createCyborg(int factor)
+void enemy::createRobot(int factor)
 {
     int healthValue=(((factor + 3)^2) - 10)*2;
     int damageValue=(((factor)^2 / 2) + ((5 * factor) / 2))*.5;
     setMaxHealth(healthValue);
     setHealth(getMaxHealth());
-    setMapRep('C');
-    setCharacterName("Cyborg");
+    setMapRep('R');
+    setCharacterName("Robot");
     setBaseDamage(damageValue);
     setArmor(2);
     setAccuracy(80);
     setLevel(factor);
 }
 //modified version of numOfItems created by Ashley
-int numOfEnemies(char symbolArray[500][500],vector <Location> &possiblePositions)
+int numOfEnemies(char symbolArray[500][500],vector <Location> possiblePositions)
 {
     int spaceCounter = 0;
     int enemiesToCreate = 0;
-    const int ENEMIES_PER_SPACES = 20;
-
-    for(int x = 0; x < 500; x++)
-    {
-        for(int y = 0; y < 500; y++)
-        {
-            if(symbolArray[x][y] == ' ')
-            {
-                if(symbolArray[x][y+1] == ' ' && symbolArray[x][y-1] == ' ')
-                {
-                    spaceCounter++;
-
-                    Location newLocation(x,y);
-                    possiblePositions.push_back(newLocation);
-                }
-                if(symbolArray[x+1][y] == ' ' && symbolArray[x-1][y] == ' ')
-                {
-                    spaceCounter++;
-
-                    Location newLocation(x,y);
-                    possiblePositions.push_back(newLocation);
-                }
-            }
-        }
-    }
-
+    int enemiesPerSpaces = (rand()%20)+35;
     //1 enemy for every 20 spaces
-    enemiesToCreate = spaceCounter/ENEMIES_PER_SPACES;
+    enemiesToCreate = spaceCounter/enemiesPerSpaces;
     //This guarantees at least 1 enemy to spawn in a given room
     if(enemiesToCreate==0)
     {
@@ -83,7 +66,7 @@ int numOfEnemies(char symbolArray[500][500],vector <Location> &possiblePositions
 }
 void spawnEnemies(char symbolArray[500][500],vector <Location> &possiblePositions,int enemiesToCreate,player mainChar)
 {
-    for(int i;i>enemiesToCreate;i++)
+    for(int i=0;i>enemiesToCreate;i++)
     {
       //determine enemy type
     const int TYPES_OF_ENEMIES=3;
